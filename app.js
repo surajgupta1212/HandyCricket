5e901b31f1d2b7eb823f1b93d998d947d27819f7
krishnam <maheshwarikrishnam123@gmail.com>
2024-06-01 17:31:32 +0530

    initial project setup

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
-// Backend server
+const express = require("express");
+const socket = require("socket.io");
+const http = require("http");
+const path = require("path");
+require('dotenv').config();
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+const server = http.createServer(app);
+const io = socket(server);
+
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+// io logic
+io.on("connection", (socket) => {
+  console.log("New connection ...");
+});
+
+server.listen(PORT, () => {
+  console.log(`Server is running on port http://localhost:${PORT}`);
+});
