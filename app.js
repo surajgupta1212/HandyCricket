6b1365b5980c1babbf9e7db9b06df81ba1bf8170
krishnam <maheshwarikrishnam123@gmail.com>
2024-06-04 08:42:12 +0530

    ADD: server and client script for game. ( locally tested )

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const express = require("express");
 const socket = require("socket.io");
 const http = require("http");
 const path = require("path");
-require('dotenv').config();
+const crypto = require("crypto");
+require("dotenv").config();
 
 const PORT = process.env.PORT || 3000;
 
@@ -17,11 +18,112 @@ app.get("/", (req, res) => {
   res.render("index");
 });
 
-// io logic
+const queue = [];
+const rooms = {};
+
+function generateRoomId(id) {
+  return crypto.createHash("sha256").update(id).digest("hex").substring(0, 8);
+}
+
 io.on("connection", (socket) => {
-  console.log("New connection ...");
+  console.log(`[connected] : ${socket.id}`);
+
+  socket.on("createRoom", () => {
+    const roomId = generateRoomId(socket.id);
+    socket.join(roomId);
+    socket.emit("socketId", socket.id, roomId);
+    rooms[roomId] = { players: [socket.id], choices: {} };
+    socket.emit("roomCreated", roomId);
+    console.log(`[room created] : ${roomId}`);
+  });
+
+  socket.on("joinRoom", (roomId) => {
+    if (rooms[roomId] && rooms[roomId].players.length === 1) {
+      socket.join(roomId);
+      socket.emit("socketId", socket.id, roomId);
+      rooms[roomId].players.push(socket.id);
+      console.log(`[room joined] : ${socket.id} joined ${roomId}`);
+      io.to(roomId).emit("startToss", rooms[roomId].players);
+    } else if (rooms[roomId]) {
+      socket.emit("error", "Room is full");
+    } else {
+      socket.emit("error", "Room doesn't exist");
+    }
+  });
+
+  socket.on("joinRandom", () => {
+    if (queue.length > 0) {
+      const { socketId, roomId } = queue.shift();
+      socket.join(roomId);
+      socket.emit("socketId", socket.id, roomId);
+      console.log(`[room joined] : ${socket.id} joined ${roomId}`);
+      rooms[roomId].players.push(socket.id);
+      io.to(roomId).emit("startToss", rooms[roomId].players);
+    } else {
+      const roomId = generateRoomId(socket.id);
+      socket.join(roomId);
+      socket.emit("socketId", socket.id, roomId);
+      console.log(`[waiting] : ${socket.id} is waiting in ${roomId}`);
+      rooms[roomId] = { players: [socket.id], choices: {} };
+      queue.push({ socketId: socket.id, roomId });
+      socket.emit("waiting");
+    }
+  });
+
+  socket.on("tossCall", (roomId, call) => {
+    const tossResult = Math.random() < 0.5 ? "heads" : "tails";
+    const winner =
+      call === tossResult
+        ? socket.id
+        : rooms[roomId].players[0] === socket.id
+        ? rooms[roomId].players[1]
+        : rooms[roomId].players[0];
+    io.to(roomId).emit("tossResult", { tossResult, tossWinner: winner });
+  });
+
+  socket.on("start", (roomId, choice) => {
+    io.to(roomId).emit("startGame", { id: socket.id, choice });
+  });
+
+  socket.on("choice", (roomId, choice) => {
+    const room = rooms[roomId];
+    if (room && room.players.includes(socket.id)) {
+      room.choices[socket.id] = choice;
+      if (Object.keys(room.choices).length === 2) {
+        io.to(roomId).emit("choicesMade", room.choices);
+        room.choices = {};
+        setTimeout(() => {
+          io.to(roomId).emit("choose");
+        }, 2000);
+      }
+    }
+  });
+
+  socket.on("disconnect", () => {
+    const roomIndex = queue.findIndex((room) => room.socketId === socket.id);
+    if (roomIndex !== -1) {
+      queue.splice(roomIndex, 1);
+      console.log(
+        `[disconnected] : ${socket.id} is disconnected - ${queue[roomIndex]?.roomId}`
+      );
+    } else {
+      for (const room in rooms) {
+        if (
+          rooms.hasOwnProperty(room) &&
+          rooms[room].players.includes(socket.id)
+        ) {
+          console.log(
+            `[disconnected] : ${socket.id} is disconnected - ${room}`
+          );
+          io.to(room).emit("opponentDisconnected");
+          delete rooms[room];
+          break;
+        }
+      }
+    }
+  });
 });
 
 server.listen(PORT, () => {
-  console.log(`Server is running on port http://localhost:${PORT}`);
+  console.log(`>> Server is running on port http://localhost:${PORT}`);
 });
--- public/scripts/game.js
+++ public/scripts/game.js
@@ -1,1 +1,135 @@
+const playerName = prompt("HandyCricket\nEnter your name : ");
+let myId,
+  opponentId,
+  roomId,
+  batter,
+  baller,
+  score = 0,
+  target = 0,
+  inning = 1;
+console.log(`Your name = ${playerName}`);
+
 const socket = io();
+
+document.getElementById("createRoom").addEventListener("click", () => {
+  socket.emit("createRoom");
+});
+
+document.getElementById("joinRoom").addEventListener("click", () => {
+  const roomID = prompt("Enter Room ID:");
+  socket.emit("joinRoom", roomID);
+});
+
+document.getElementById("joinRandom").addEventListener("click", () => {
+  socket.emit("joinRandom");
+});
+
+const urlParams = new URLSearchParams(window.location.search);
+const roomID = urlParams.get("room");
+if (roomID) {
+  socket.emit("joinRoom", roomID);
+  const baseUrl = window.location.href.split("?")[0];
+  history.replaceState({}, document.title, baseUrl);
+}
+
+socket.on("roomCreated", (roomID) => {
+  console.log(
+    `Room created. Share this link with your friend to join: ${window.location.origin}?room=${roomID}`
+  );
+});
+
+socket.on("socketId", (id, room) => {
+  myId = id;
+  roomId = room;
+});
+
+socket.on("waiting", () => {
+  console.log("waiting...");
+});
+
+socket.on("startToss", (players) => {
+  if (myId === players[0]) {
+    opponentId = players[1];
+    const tossCall = prompt("heads/tails:");
+    socket.emit("tossCall", roomId, tossCall);
+  } else {
+    opponentId = players[0];
+    console.log("[TOSS] opponent is choosing!");
+  }
+});
+
+socket.on("tossResult", (data) => {
+  console.log(`It's a ${data.tossResult}`);
+  console.log(data.tossWinner);
+  if (data.tossWinner === myId) {
+    console.log(`you won the toss.`);
+    const choice = prompt("choose bat/ball:");
+    console.log(`I choose to ${choice}`);
+    socket.emit("start", roomId, choice);
+  } else {
+    console.log(`opponent won the toss.`);
+  }
+});
+
+socket.on("startGame", (data) => {
+  if (
+    (data.id === myId && data.choice === "bat") ||
+    (data.id === opponentId && data.choice === "ball")
+  ) {
+    batter = myId;
+    baller = opponentId;
+  } else {
+    batter = opponentId;
+    baller = myId;
+  }
+  const choice = prompt("[1-6]:");
+  socket.emit("choice", roomId, choice);
+});
+
+socket.on("choose", () => {
+  const choice = prompt("[1-6]:");
+  socket.emit("choice", roomId, choice);
+});
+
+socket.on("choicesMade", (choices) => {
+  console.log(
+    `Your choice: ${choices[myId]}, Opponent's choice: ${choices[opponentId]}`
+  );
+  if (inning === 1) {
+    if (choices[myId] === choices[opponentId]) {
+      console.log("[OUT]");
+      console.log("[Target] : ", score);
+      target = score;
+      score = 0;
+      [batter, baller] = [baller, batter];
+      inning = 2;
+    } else {
+      score += Number(choices[batter]);
+      console.log(`>> score : ${score}`);
+    }
+  } else {
+    if (choices[myId] === choices[opponentId]) {
+      console.log("[OUT]");
+      console.log(`>> target : ${target}`);
+      console.log(`>> score : ${score}`);
+      if (myId === batter) alert("GameOver! You lose");
+      else alert("GameOver! You won");
+    } else {
+      score += Number(choices[batter]);
+      console.log(`>> target : ${target}`);
+      console.log(`>> score : ${score}`);
+      if (score >= target) {
+        if (myId === batter) alert("GameOver! You won");
+        else alert("GameOver! You lose");
+      }
+    }
+  }
+});
+
+socket.on("error", (message) => {
+  console.log(`Error : ${message}`);
+});
+
+socket.on("opponentDisconnected", () => {
+  alert("Your opponent has disconnected. The game is over.");
+});
